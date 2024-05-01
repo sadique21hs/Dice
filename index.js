a5160596d02ea5cd132f3622e48cd015d0f1ad29
Md Sadique Hossain <sadique21.hossain@gmail.com>
2024-05-01 16:34:23 +0600

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ var image1=document.querySelectorAll("img")[0];
 image1.setAttribute("src" , randomImagesSource);
 
 
+//For 2nd img
+
 var randomnumber2= Math.floor(Math.random()*6) + 1;
 
 var randomImages="dice"+randomnumber2+".png";
 
+var randomImagesSource="Images/"+randomImages;
+
+var image2=document.querySelectorAll("img")[1];
+
+image2.setAttribute("src" , randomImagesSource);
