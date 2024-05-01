4769d872c4f25b2c39958acffef4e194d212e499
sadique21hs <sadique21.hossain@gamil.com>
2024-05-01 16:32:03 +0600

    Updated

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var randomnumber1= Math.floor(Math.random()*6) + 1;
 
 var randomImages="dice"+randomnumber1+".png";
 
-var randomImagesSource="Images"+randomImages;
+var randomImagesSource="Images/"+randomImages;
 
-var image1=document.querySelectorAll("img1")[0];
+var image1=document.querySelectorAll("img")[0];
 
 image1.setAttribute("src" , randomImagesSource);
